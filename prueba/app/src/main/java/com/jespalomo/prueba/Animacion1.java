9350bcc739d6f2bdf8f6b98a0b965cbc3c839340
jespalomo <jespalomov@gmail.com>
2021-08-12 20:54:31 +0200

    animacion inicial hecha

---
+++ prueba/app/src/main/java/com/jespalomo/prueba/Animacion1.java
@@ -0,0 +1,40 @@
+package com.jespalomo.prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Animacion1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_animacion1);
+
+        Animation anim1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation anim2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView titulo = (TextView)findViewById(R.id.tituloApp);
+        ImageView logo = (ImageView) findViewById(R.id.imagenLogo);
+
+        titulo.setAnimation(anim2);
+        logo.setAnimation(anim1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent next = new Intent(Animacion1.this, Seleccion.class);
+                startActivity(next);
+                finish();
+            }
+        }, 4000);
+    }
+}
